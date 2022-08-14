05eac2db9414fa47507e23c0aacc3bf242ab41b7
Abhishek S Nikam <abhishekn2@bitwiseglobal.net>
2022-08-14 18:59:04 +0530

    first commit

---
+++ 6-module.js
@@ -0,0 +1,8 @@
+//module
+const names=require('./4-names');
+const sayHi=require('./5-functions');
+const data=require('./7-alternative-flavour');
+//console.log(data)
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
